18101dc0c905bd3e1f8c4da1cba90dcba4474da2
Andrea Talenti <23279528+RenzoTale88@users.noreply.github.com>
2022-02-15 11:27:36 +0000

    Replace boxplot with density plot

--- Code/SizeAndSupportAnalysis/T-test_sizes.R
+++ Code/SizeAndSupportAnalysis/T-test_sizes.R
@@ -11,21 +11,15 @@ a <- read_table(args[1],
                                  Size = 'n') ) %>%
   mutate(AbsSize=abs(Size))
 
-p1 <- ggboxplot(a, "Support", "AbsSize",
-          color = "black", fill = "gray") +
-          scale_y_log10()+ 
-          ylab('Structural Variant absolute log10(size)') +
-          xlab('Number of individuals supporting a structural variant')
-ggsave(filename = paste0(args[2], 'Boxplot_size_by_support.pdf'), plot = p1, device = 'pdf', width = 16, height = 9, dpi = 300)
-
 b <- a
-b[b$Support>1, 'Support'] <- 2
-p2 <- ggboxplot(b, "Support", "AbsSize",
-          color = "black", fill = "gray") +
-          scale_y_log10()+ 
-          ylab('Structural Variant absolute log10(size)') +
-          xlab('Number of individuals supporting a structural variant')
-ggsave(filename = paste0(args[2], '/Boxplot_size_by_support_twoGroups.pdf'), plot = p2, device = 'pdf', width = 16, height = 9, dpi = 300)
+b$Support = as.character(b$Support)
+b[b$Support != '1', 'Support'] = '>1'
+b$Support = factor(b$Support, levels = c('1', '>1'))
+
+p <- ggdensity(b, x="AbsSize", add = "mean", rug = TRUE, fill = "Support", color = "Support", palette = c("#00AFBB", "#E7B800")) + 
+          scale_x_log10()+ 
+          xlab('Structural Variant absolute log10(size)') 
+ggsave(filename = paste0(args[2], '/SupplementaryFigure1.pdf'), plot = p, device = 'pdf', width = 16, height = 9, dpi = 300)
 
 supp1 <- pull(a[a$Support==1,'AbsSize'])
 supp2 <- pull(a[a$Support>1,'AbsSize'])
